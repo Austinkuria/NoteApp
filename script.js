08d53a659bbd2cba509cd6a3c7765f02c6831f59
Austin Maina <101550498+Austinkuria@users.noreply.github.com>
2024-06-02 21:44:06 +0300

    Added functionality for editing and deleting notes: Implemented edit and delete buttons in the UI, along with corresponding JavaScript functions for managing these actions

--- script.js
+++ script.js
@@ -16,20 +16,57 @@ function displayNotes(){
     notesArray.innerHTML = '';
     // iterate over myNotes
     myNotes.forEach((note)=>{
-        // create a div to hold the note
+        // create a div element for single note card
         const singleNoteCard = document.createElement('div');
         // add classes to the div
         singleNoteCard.classList.add('card', 'mb-3');
-        // create a div to hold the note content
+        // create a div element for the note card body
         const singleNoteCardBody = document.createElement('div');
         // add classes to the div
-        singleNoteCardBody.classList.add('card-body');
-        // add the note content to the div
-        singleNoteCardBody.textContent = note;
-        // append the note content div to the note div
+        singleNoteCardBody.classList.add('card-body','d-flex','justify-content-between');
+        // create a paragraph element for the single note card content
+        const singleNoteCardContent = document.createElement('p');
+        // add classes to the paragraph
+        singleNoteCardContent.classList.add('card-text','fs-5');
+        // add note content to the paragraph 
+        singleNoteCardContent.textContent = note;
+        // append single note card body to the single note card
         singleNoteCard.appendChild(singleNoteCardBody);
-        // append the note div to the notesArray div
+        // append the single note card content to the single note card body
+        singleNoteCardBody.appendChild(singleNoteCardContent);
+        // append the single note card to the notesArray div
         notesArray.appendChild(singleNoteCard);
+        // create a div element for the  single note card buttons
+        const singleNoteCardBodyButtons = document.createElement('div');
+        // add classes to the div
+        singleNoteCardBodyButtons.classList.add('btns-group','mx-3');
+        // append the single note card buttons to the single note card body
+        singleNoteCardBody.appendChild(singleNoteCardBodyButtons);
+        //create edit button
+        const editButton = document.createElement('button');
+        // add text content to the button
+        editButton.textContent = 'Edit';
+        // add classes to the button
+        editButton.classList.add('btn', 'btn-primary','edit-button');
+        // add event listener to the button
+        editButton.addEventListener('click',()=>{
+            // call the editSingleNoteCardContent function and pass the index of the note
+            editSingleNoteCardContent(myNotes.indexOf(note));
+        });
+        singleNoteCardBodyButtons.appendChild(editButton);
+        // create a delete button
+        const deleteButton =  document.createElement('button');
+        // add text content to the button
+        deleteButton.textContent = 'Delete';
+        // add classes to the button
+        deleteButton.classList.add('btn', 'btn-danger', 'delete-button');
+        // add event listener to the button
+        deleteButton.addEventListener('click',()=>{
+             // call the deleteSingleNoteCardContent function and pass the index of the note
+            deleteSingleNoteCardContent(myNotes.indexOf(note));
+        })
+        // append the delete button to the single note card buttons
+        singleNoteCardBodyButtons.appendChild(deleteButton);
     });
     // display the notesArray div
     notesArray.style.display = 'block';
@@ -37,13 +74,14 @@ function displayNotes(){
 
 //Function to save a new note
 function saveNote(){
+    // get the note content
     const noteContent = document.getElementById('note-content').value;
     // check if the note is not empty
     if(noteContent.trim() !== ''){
         // add the note to the array        
         myNotes.push(noteContent);
         // save the note to local storage
-        localStorage.setItem('notes', JSON.stringify(myNotes.join()));
+        localStorage.setItem('notes', myNotes.join(separator));
         // alert('Note added successfully');
         // clear the input field
         document.getElementById('note-content').value = '';
@@ -59,8 +97,29 @@ function saveNote(){
 // display existing notes
 displayNotes();
   // add event listener for the 'Enter' key press
-  document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function(event) {
+    // check if the key pressed is 'Enter'
     if (event.key === 'Enter') {
         saveNote();
     }
 });
+
+// function to delete a note
+function deleteSingleNoteCardContent(index) {
+    // remove the note from the array using .splice(index,number of items to be rempved ) method-index is the position of the note in the array
+    myNotes.splice(index, 1);
+    // save the notes to local storage
+    localStorage.setItem('notes', myNotes.join(separator));
+    // display the notes
+    displayNotes();
+}
+
+// function to edit a note
+function editSingleNoteCardContent(index){
+    // get the note content
+    document.getElementById('note-content').value = myNotes[index];
+    // display the modal
+    const modal = new bootstrap.Modal(document.getElementById('newNoteModal'));
+    // show the modal
+    modal.show();
+}
